a45f841e68b98146a3bc4a72a6116051b062f11f
Nicolas Berger <nicoberger@gmail.com>
2012-07-28 17:28:17 -0300

    Adds province and city to api response

--- lib/rapi_pago_finder.rb
+++ lib/rapi_pago_finder.rb
@@ -3,7 +3,6 @@ require 'pry'
 
 class RapiPagoFinder
   def self.all(options = {})
-    agent = Mechanize.new
     page = agent.get("http://www.rapipago.com.ar/rapipagoWeb/index.htm")
     search_form = find_form(page)
     province = options[:province]
@@ -14,24 +13,51 @@ class RapiPagoFinder
     search_form.field_with(:name => "ciudadSuc").options.detect {|opt| opt.value.downcase == city.downcase}.select
     page = agent.submit(search_form)
 
-    addresses = grab_addresses(page)
+    addresses = []
 
+    each_page_from(page) do |page, page_num|
+      addresses.concat(grab_addresses(page))
+    end
+
+    # add city and province
+    addresses.map {|a| a.merge(:province => province, :city => city)}
+  end
+
+protected
+  def self.agent
+    @agent ||= Mechanize.new
+  end
+
+  def self.each_page_from(page)
+    # current page
+    yield page, 1
+
+    # following pages
     page.links_with(:href => /doPage/).each do |l|
       page_num = l.text.to_i
+      # skip first page
       next if page_num <= 1
+
       search_form = find_form(page)
       search_form.action += "?pageNum=#{page_num}"
 
       page = agent.submit(search_form)
-      addresses.concat(grab_addresses(page))
-    end
 
-    addresses
+      yield page, page_num
+    end
   end
 
-protected
   def self.grab_addresses(page)
-    page.search(".resultadosText").map(&:text).reject {|a| a =~ /Ver mas/}.map {|a| {:address => a}}
+    streets_and_numbers = page.search(".resultadosText").map(&:text).reject {|a| a =~ /Ver mas/}
+    names = page.search(".resultadosTextWhite").map(&:text)
+    ids = page.search(".resultadosNumeroSuc").map(&:text)
+    streets_and_numbers.zip(names, ids).map do |street_and_number, name, id|
+      {
+        :street_and_number => street_and_number,
+        :name => name,
+        :agency_id => id
+      }
+    end
   end
 
   def self.find_form(page)
--- spec/requests/rapipago_list_spec.rb
+++ spec/requests/rapipago_list_spec.rb
@@ -11,10 +11,16 @@ describe "rapi_pago_list" do
   end
 
   it "gives address for some rapi pago stores" do
-    @rapipagos.map {|rp| rp["address"]}.should include("AV LA PLATA 537")
-    @rapipagos.map {|rp| rp["address"]}.should include("M BRAVO 7")
+    @rapipagos.map {|rp| rp["street_and_number"]}.should include("AV LA PLATA 537")
+    @rapipagos.map {|rp| rp["street_and_number"]}.should include("M BRAVO 7")
   end
   it "gives address for some rapi pago stores in page 2" do
-    @rapipagos.map {|rp| rp["address"]}.should include("AV B RIVADAVIA 4225")
+    @rapipagos.map {|rp| rp["street_and_number"]}.should include("AV B RIVADAVIA 4225")
+  end
+  it "includes province for each address" do
+    @rapipagos[0]["province"].should == "capital_federal"
+  end
+  it "includes city for each address" do
+    @rapipagos[0]["city"].should == "almagro"
   end
 end
